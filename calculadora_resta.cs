88515e03b021d9c67b5631a543298948b261e05d
bcsebas98 <107517056+bcsebas98@users.noreply.github.com>
2022-11-01 20:51:01 -0500

    Update and rename calculadora.cs to calculadora_resta.cs
    Se separo la clase de división y resta

---
+++ calculadora_resta.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class calculadora
+{
+Agregar_resta
+	private int num1;
+	private int num2;
+
+	public numero()
+	{
+	}
+
+	public global::System.Int32 Num1 { get => num1; set => num1 = value; }
+	public global::System.Int32 Num2 { get => num2; set => num2 = value; }
+
+	public int resta()
+	{
+		return num1 - num2;
+	}
+   
+}
