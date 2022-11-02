973b99bd66066f642e33caa136e3a8d2dbcd22ea
elena2900 <helensita.palma1@gmail.com>
2022-11-01 20:06:32 -0500

    Agregue la operacion resta

---
+++ calculadora.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class calculadora
+{
+	private int num1;
+	private int num2;
+
+	public numero()
+	{
+	}
+
+	public global::System.Int32 Num1 { get => num1; set => num1 = value; }
+	public global::System.Int32 Num2 { get => num2; set => num2 = value; }
+
+	public int resta()
+	{
+		return num1 - num2;
+	}
+}
