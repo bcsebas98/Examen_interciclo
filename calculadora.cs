e3e21bb2e274ac532f864cf12fda9f288a3f63fc
bcsebas98 <bcsebas1998@gmail.com>
2022-11-01 20:03:41 -0500

    Agregue la operación división

---
+++ calculadora.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class calculadora
+{
+    private int num1;
+    private int num2;
+
+    public numero()
+	{
+    }	
+	public global::System.Int32 Num1 { get => num1; set => num1 = value; }
+	public global::System.Int32 Num2 { get => num2; set => num2 = value; }
+
+    public int dividir()
+    {
+        if (num2 != 0)
+        {
+            return num1 / num2;
+        }
+        else
+        {
+        }
+    }
+
+}
